ecf7f95400a4ac60d2f78667067bd5a385b8ccaa
CodedStrawhat <vspanwar1@gmail.com>
2021-02-24 14:43:49 +0530

    makes a directory and stores gathered links in a file

--- web_crawler.py
+++ web_crawler.py
@@ -1,8 +1,9 @@
 from bs4 import * # To find the hyperlinks
 import requests # To make http requests to a site
 from argparse import * #To make a CLI
-
-
+import os 
+import subprocess
+import re
 
 parser=ArgumentParser()
 parser.add_argument("url",help="Enter the URL you want to scrape")
@@ -12,26 +13,37 @@ p=parser.parse_args()
 '''The usearch function searches the html source code for
 links and recursively searches further for links based on
 given depth'''
-
+l=[]
 def usearch(u,depth):
     if depth==int(p.depth):
         return 0
     r=requests.get(u)
     soup=BeautifulSoup(r.content,'html.parser')
     links= soup.find_all('a')
-    l=set()
+    
     for link in links:
         link=link.get('href')
         if link!='#' and link!=None:
-            l.add(link)
+            l.append(link)
     for link in l:
         if ("https://" or "http://")not in link:
-            print(u+link)
+            link = p.url+link
+            if link in l:
+                continue
+            f.write(link+"\n")
         else:
-            print(link)
+            if link in l:
+                continue
+            f.write(link+"\n")
             usearch(link,depth+1)
-            
+
+title=re.split("www.",p.url)
+title=title[1]
+os.system("mkdir {}".format(title)) 
+os.system("touch {}/domains.txt".format(title))
+f= open("{}/domains.txt".format(title),"a")
+
 usearch(p.url,0)
-                
+f.close()              
     
     
