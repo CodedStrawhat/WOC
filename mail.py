c9199e0bbc1d2c2f67f5ee3caddb3b899d2f86e0
CodedStrawhat <vspanwar1@gmail.com>
2021-03-05 02:58:24 +0530

    Added contact harvesting and command line options

--- mail.py
+++ mail.py
@@ -1,16 +1,14 @@
-import os
 import re 
 import requests
-def harv(dir,u):
-    os.system("touch {}/emails.txt".format(dir))
-    f= open("{}/emails.txt".format(dir),"a")
+
+def harv(u,fil_1):
     r=requests.get(u)
     EMAIL_REGEX =  r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
     l=set() 
-    for re_match in re.finditer(EMAIL_REGEX, r.text):
-        l.add(re_match.group())
+    for match in re.finditer(EMAIL_REGEX, r.text):
+        l.add(match.group())
     for i in l:
-        f.write(i)
+        fil_1.write(i+'\n')
 
 
 
--- web_crawler.py
+++ web_crawler.py
@@ -1,54 +1,62 @@
-from bs4 import * # To find the hyperlinks
-import requests # To make http requests to a site
-from argparse import * #To make a CLI
+from bs4 import * # To find the hyperlinks 
+import requests # To make http requests to a site 
+from argparse import * #To make a CLI 
 import os 
-import re
-import mail
+import re 
+import mail 
+import contacts
 from urllib.parse import urlparse 
 
-parser=ArgumentParser()
-parser.add_argument("url",help="Enter the URL you want to scrape")
-parser.add_argument("depth",help="Enter the depth for search")
-p=parser.parse_args()
+parser=ArgumentParser() 
+parser.add_argument("url",help="Enter the URL you want to scrape") 
+parser.add_argument("depth",help="Enter the depth for search") 
+parser.add_argument("-e",action = 'store_true',help="Option for email harvesting") 
+parser.add_argument("-c",action = 'store_true',help="Option for contact harvesting")
+p=parser.parse_args() 
 
 '''The usearch function searches the html source code for
 links and recursively searches further for links based on
 given depth'''
+
 l=[]
 
 def usearch(u,depth):
-    mail.harv(title,u)
+    if p.e:
+        mail.harv(u,fil_1)
+    if p.c:
+        contacts.harv(u,fil_2)
     if depth==int(p.depth):
         return 0
     r=requests.get(u)
     soup=BeautifulSoup(r.content,'html.parser')
     links= soup.find_all('a')
-    
     for link in links:
         link=link.get('href')
         if link!='#' and link!=None:
-            l.append(link)
+            if ("https://" or "http://")not in link:
+                link = p.url+link
+            if link not in l:
+                l.append(link)
+    
     for link in l:
-        if ("https://" or "http://")not in link:
-            link = p.url+link
-            if link in l:
-                continue
-            f.write(link+"\n")
-        else:
-            if link in l:
-                continue
-            f.write(link+"\n")
-            usearch(link,depth+1)
-
+        f.write(link+"\n")
+        usearch(link,depth+1)
 
 title=urlparse(p.url)
 title=title.netloc
 os.system("mkdir {}".format(title)) 
 os.system("touch {}/domains.txt".format(title))
 f= open("{}/domains.txt".format(title),"a")
-
+if p.e:
+    os.system("touch {}/emails.txt".format(title))
+    fil_1= open("{}/emails.txt".format(title),"a")
+if p.c:
+    os.system("touch {}/contacts.txt".format(title))
+    fil_2= open("{}/contacts.txt".format(title),"a")
 usearch(p.url,0)
-
-f.close()              
-
+f.close()
+if p.e:
+    fil_1.close()         
+if p.c:
+    fil_2.close()
     
