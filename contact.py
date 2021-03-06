6293fee891f29e9a183fe4e77d212cf7eb781486
CodedStrawhat <vspanwar1@gmail.com>
2021-03-06 12:46:47 +0530

    contact regex edited

--- contact.py
+++ contact.py
@@ -3,7 +3,7 @@ import requests
 
 def harv(u,fil_2,l,no_write):
     r=requests.get(u)
-    reg=r"\+91\d{10}|\d{10}"
+    reg=r"(\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14})|d{10}"
     for match in re.finditer(reg, r.text):
         l.add(match.group())
     ld=l.difference(no_write)
